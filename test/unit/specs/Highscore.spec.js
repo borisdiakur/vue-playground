beafc69a886349b1842f13dee296c5948cfa2c8c
Boris Diakur <contact@borisdiakur.com>
2017-02-03 15:22:51 +0100

    using a better coverage reporter; added shallow specs

---
+++ test/unit/specs/Highscore.spec.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Highscore from 'src/components/Highscore'
+
+describe('Highscore.vue', () => {
+  it('should render correct contents', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(Highscore)
+    })
+    expect(vm.$el.querySelector('.highscore h1').textContent)
+      .to.equal('Highscore')
+  })
+})
