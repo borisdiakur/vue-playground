0b28e281e60cc4e3b93dea88f9f8b10c02fe269e
Daniel Funiok <daniel.funiok@aoe.com>
2017-02-06 15:01:09 +0100

    Highscore table added with dummy data

--- src/components/highscore/Highscore.vue
+++ src/components/highscore/Highscore.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="highscore">
-    <h1>Highscore</h1>
-    <ul>
-    </ul>
+    <h1>Highscores</h1>
+    <p>{{data}}</p>
+    <table id="hs-list">
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in items">
+        <td>{{ index }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.score }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'highscore'
+  name: 'highscore',
+  data () {
+    return {
+      items: [
+          { name: 'Charles', score: '9999' },
+          { name: 'Jean', score: '8888' },
+          { name: 'Iceman', score: '7777' },
+          { name: 'Storm', score: '6666' }
+      ]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
