5a01edd193dc94ad94d26bb8e7910d864ecf32b0
Boris Diakur <contact@borisdiakur.com>
2017-02-09 18:01:10 +0100

    next (still buggy)

--- src/components/playground/info/Info.vue
+++ src/components/playground/info/Info.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="info">
+    <next></next>
   </div>
 </template>
 
 <script>
+import Next from './next/Next'
+import Hints from './hints/Hints'
+import Display from './display/Display'
+
 export default {
-  name: 'info'
+  name: 'info',
+  components: {
+    Next,
+    Hints,
+    Display
+  }
 }
 </script>
 
--- src/components/playground/info/next/Next.vue
+++ src/components/playground/info/next/Next.vue
@@ -1,12 +1,77 @@
 <template>
   <div class="next">
+    lalalal
+  </div>
+</template>
 
+<template>
+  <div class="grid"
+       v-bind:style="{width: 22 * rows[0].length}">
+    <ul>
+      <li v-for="row in rows"
+          class="row">
+        <ul>
+          <li v-for="cell in row"
+              class="cell"
+              v-bind:class="{
+                'tetrimino-i': cell.tetriminoI,
+                'tetrimino-j': cell.tetriminoJ,
+                'tetrimino-l': cell.tetriminoL,
+                'tetrimino-o': cell.tetriminoO,
+                'tetrimino-s': cell.tetriminoS,
+                'tetrimino-t': cell.tetriminoT,
+                'tetrimino-z': cell.tetriminoZ
+              }"></li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Tetrimino from 'store/modules/grid/tetrimino'
+
+// initialize empty grid (initial state)
+const GRID_HEIGHT = 4
+const GRID_WIDTH = 4
+const rows = []
+for (let i = GRID_HEIGHT; i--;) {
+  const row = []
+  for (let j = GRID_WIDTH; j--;) {
+    row.push({}) // cell
+  }
+  rows.push(row)
+}
+
 export default {
-  name: 'next'
+  name: 'next',
+  data () {
+    return {
+      next: this.$store.state.playground.next
+    }
+  },
+  computed: {
+    rows () {
+      const tetrimino = new Tetrimino(this.$store.state.playground.next, {x: 0, y: 0})
+      const matrix = tetrimino.matrix
+      const rows = []
+      for (let i = 0; i < matrix.length; i++) {
+        rows.push([])
+        const row = matrix[i]
+        for (let j = 0; j < row.length; j++) {
+          const cell = row[j]
+          if (cell === 1) {
+            const gridCell = {}
+            gridCell[`tetrimino${tetrimino.type}`] = true
+            rows[i][j] = gridCell
+          } else {
+            rows[i][j] = {}
+          }
+        }
+      }
+      return rows
+    }
+  }
 }
 </script>
 
